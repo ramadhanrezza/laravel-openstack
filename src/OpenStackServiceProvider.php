edd35b08c583ea7cdec7619a5c47480f85c128f1
ramadhanrezza <rezza_ramdhan@biznetgio.com>
2018-03-19 14:09:21 +0700

    Initial Commit

---
+++ src/OpenStackServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Sarjanakomedi\OpenStack;
+
+use Illuminate\Support\ServiceProvider;
+
+class OpenStackServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
